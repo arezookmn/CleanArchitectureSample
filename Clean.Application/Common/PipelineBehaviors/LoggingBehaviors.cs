a6a1239741c82bbb3a106081ac843db55f152793
arezookmn <arezookmn1@gmail.com>
2024-03-29 12:39:10 +0330

    Add Logging Behavior to PipelineBehavior

---
+++ Clean.Application/Common/PipelineBehaviors/LoggingBehaviors.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Clean.Application.Common.PipelineBehaviors;
+public class LoggingBehavior<TRequest, TResponse>(ILogger<TRequest> logger)
+: IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly ILogger _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation(" Request: {@Request}",
+             request);
+
+        var response = await next();
+
+        _logger.LogInformation(" Response: {requestName} {@Response}", requestName, response);
+        return response;
+    }
+}
--- Clean.Application/ServiceCollectionExtentions.cs
+++ Clean.Application/ServiceCollectionExtentions.cs
@@ -3,6 +3,7 @@ using Mapster;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using FluentValidation;
 
 namespace Clean.Application;
 public static class ServiceCollectionExtentions
@@ -15,10 +16,13 @@ public static class ServiceCollectionExtentions
 
     public static IServiceCollection AddMediatorConfiguration(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg =>
         { 
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
         });
 
         return services;
